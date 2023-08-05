5eeac6bf2a053cfa0864bff3a3ec56621f9fddd9
Gerard Sans <gerard.sans@gmail.com>
2023-08-05 21:28:42 +0100

    Initial drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'vertex-ai-palm2-angular';
+  messages = <any>[];
+  loading = false;
+
+  // Random ID to maintain session with server
+  sessionId = Math.random().toString(36).slice(-5);
+
+  constructor(public http: HttpClient) { 
+  }
+
+  ngOnInit(): void {
+    this.addBotMessage('Human presence detected �. How can I help you? ');
+
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${environment.GCLOUD_AUTH_PRINT_ACCESS_TOKEN}`);
+    const options = {
+      "headers": headers,
+    }
+
+    const body = {
+      "instances": [
+        {
+          "prompt": "Write a recipe for a chocolate cake."
+        }
+      ],
+      "parameters": {
+        "temperature": 0.2,
+        "maxOutputTokens": 100,
+        "topP": 0.95,
+        "topK": 40
+      }
+    };
+    const url = `https://us-central1-aiplatform.googleapis.com/v1/projects/${environment.PROJECT_ID}/locations/us-central1/publishers/google/models/text-bison:predict`;
+    // this.http.post(url, body, options).subscribe(Response => {
+    //   console.log(Response)
+    // });
+  }
+
+
+  handleUserMessage(event:any) {
+    console.log(event);
+    const text = event.message;
+    this.addUserMessage(text);
+
+    this.loading = true;
+
+    // Make an HTTP Request
+      this.addBotMessage("Done!");
+      this.loading = false;
+  }
+  // Helpers
+  addUserMessage(text:string) {
+    this.messages.push({
+      text,
+      sender: 'You',
+      reply: true,
+      date: new Date()
+    } as any);
+  }
+
+  addBotMessage(text:string) {
+    this.messages.push({
+      text,
+      sender: 'Bot',
+      date: new Date()
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NbThemeModule, NbLayoutModule, NbChatModule, NbSpinnerModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbChatModule,
+    NbSpinnerModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
