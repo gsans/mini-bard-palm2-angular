b819b90fc59a2232be912b4666e23ca7fb1666b1
Gerard Sans <gerard.sans@gmail.com>
2023-08-24 21:36:46 +0100

    Support for multiple versions

--- src/app/generative-ai-palm/palm.module.ts
+++ src/app/generative-ai-palm/palm.module.ts
@@ -1,11 +1,11 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule, ModuleWithProviders, InjectionToken } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { TextServiceClient } from './v1beta2/text.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-export class MakerSuiteCredentials {
-  apiKEY: string = '';
-}
+export const TEXT_SERVICE_CLIENT_TOKEN = new InjectionToken<any>('TextServiceClient');
+import { MakerSuiteCredentials } from './types';
+
+import { TextServiceClient as TextServiceClient_v1beta2 } from './v1beta2/text.service';
 
 @NgModule({
   declarations: [],
@@ -20,8 +20,20 @@ export class PalmModule {
       ngModule: PalmModule,
       providers: [
         { provide: MakerSuiteCredentials, useValue: config },
-        TextServiceClient
+        {
+          provide: TEXT_SERVICE_CLIENT_TOKEN,
+          useFactory: this.textServicefactory,
+          deps: [HttpClient, MakerSuiteCredentials]
+        }
       ],
     };
+  }
+
+  private static textServicefactory(http: HttpClient, config: MakerSuiteCredentials) {
+    if (config.version.toLowerCase() === 'v1beta2') {
+      return new TextServiceClient_v1beta2(http, config);
+    } else {
+      throw new Error('Unsupported API version');
+    }
   }
 }
