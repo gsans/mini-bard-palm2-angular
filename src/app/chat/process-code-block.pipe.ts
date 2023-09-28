04397776a5222220088c0b5db95f83ad36fc0507
Gerard Sans <gerard.sans@gmail.com>
2023-09-28 22:35:57 +0100

    Fix code fence highlighting

--- src/app/chat/process-code-block.pipe.ts
+++ src/app/chat/process-code-block.pipe.ts
@@ -8,12 +8,15 @@ export class ProcessCodeBlocksPipe implements PipeTransform {
   transform(markdown: string): string {
     const processedMarkdown = markdown.replace(/```(\w*)\n([\s\S]*?)```/g, (match, language, code) => {
       const nbsp = code.replace(/ {1,}|\t+/g, (whitespace: any) => {
-        return whitespace.replace(/\s/g, '&nbsp;');
+        // replace whitespace with equivalent &nbsp; https://en.wikipedia.org/wiki/Whitespace_character#Unicode
+        return whitespace.replace(/\s/g, `\u{00A0}`);  
       });
       if (language === 'mermaid') {
         return `<div class="mermaid">${code}</div>`
       } else {
-        return `<pre><code>${nbsp}</code></pre>`;
+        return `\`\`\`${language}
+${nbsp}
+\`\`\``;
       }
     });
 
